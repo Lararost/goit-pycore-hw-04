c8f4f5b6842a485e93591d47e0cd19ba1a76c9e3
Lararost <darmorostlarisa@gmail.com>
2026-03-07 18:43:04 +0300

    Updated task1.py

--- task1.py
+++ task1.py
@@ -1,1 +1,33 @@
- 
+def total_salary(path):
+    total = 0
+    count = 0
+
+    try:
+        with open(path, "r", encoding="utf-8") as file:
+            for line in file:
+                line = line.strip()
+                if not line:
+                    continue
+
+                try:
+                    _, salary = line.split(",")
+                    total += int(salary)
+                    count += 1
+                except ValueError:
+                    print(f"Некоректний рядок у файлі: {line}")
+                    continue
+
+        average = total / count if count > 0 else 0
+        return total, average
+
+    except FileNotFoundError:
+        print("Файл не знайдено.")
+        return 0, 0
+    except OSError:
+        print("Помилка при роботі з файлом.")
+        return 0, 0
+
+
+if __name__ == "__main__":
+    total, average = total_salary("salary_file.txt")
+    print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
